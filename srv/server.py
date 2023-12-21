4602f5fefb7b58d9d4c0ce8d818c9713add825a5
Björn Brandenburg <bbb@mpi-sws.org>
2023-12-21 22:21:45 +0100

    let `make run` serve 404.html

--- srv/server.py
+++ srv/server.py
@@ -1,10 +1,24 @@
-import http.server
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import SimpleHTTPRequestHandler
 import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+
+class Handler(SimpleHTTPRequestHandler):
+    def send_error(self, code, message=None):
+        if code == 404:
+            with open("404.html", "rb") as err_msg:
+                page404 = err_msg.read()
+
+            self.send_response(code)
+            self.send_header("Connection", "close")
+            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-Length", str(len(page404)))
+            self.end_headers()
+            self.wfile.write(page404)
+        else:
+            SimpleHTTPRequestHandler.send_error(self, code, message)
+
 
 Handler.extensions_map={
     '.manifest': 'text/cache-manifest',
